ca2ae3f01367a7d5c85ed93e4e3fe5c75231705c
Amal Kalim <kalimamal07@gmail.com>
2024-05-18 15:52:29 +0100

    Time: 52 ms (17.33%) | Memory: 48.4 MB (63.83%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.java
@@ -0,0 +1,93 @@
+
+class MyHashMap {
+
+    ArrayList<LinkedList<Pair>> list = new ArrayList<>(10);
+    int size;
+    float loadFactor = 0.5f;
+
+    public MyHashMap() {
+        list = new ArrayList<>();
+        for(int i=0; i<10; i++){
+            list.add(new LinkedList<>());
+        }
+
+    }
+    
+    public void put(int key, int value) {
+        int hash = Math.abs(Integer.valueOf(key).hashCode() % list.size());
+        LinkedList<Pair> linkedList = list.get(hash);
+        for(Pair pair: linkedList){
+            if(pair.key == key) {
+                pair.value = value;
+                return;
+            }
+        }
+
+        //if((float)size/list.size()>loadFactor) this.resize();
+
+        linkedList.add(new Pair(key, value));
+
+        size++;
+        
+    }
+    
+    public int get(int key) {
+        int hash = Math.abs(Integer.valueOf(key).hashCode()%list.size());
+        LinkedList<Pair> linkedList = list.get(hash);
+        for(Pair pair: linkedList){
+            if(pair.key == key) {
+               return pair.value;
+            }
+        }
+        return -1;
+    }
+    
+    public void remove(int key) {
+        int hash = Math.abs(Integer.valueOf(key).hashCode()%list.size());
+        LinkedList<Pair> linkedList = list.get(hash);
+        Pair target = null;
+        for(Pair pair: linkedList){
+            if(pair.key == key) {
+                target = pair;
+            }
+        }
+        linkedList.remove(target);
+        size--;
+       
+    }
+
+    private void resize(){
+
+        ArrayList<LinkedList<Pair>> old = list;
+        list = new ArrayList<>();
+
+        for(int i=0; i<old.size()*2; i++){
+            list.add(new LinkedList<>());
+        }
+
+        for(int i=0; i<old.size(); i++){
+            for(Pair pair : old.get(i)){
+                put(pair.key, pair.value);
+            }
+        }
+
+    }
+
+    class Pair {
+        private int key;
+        private int value;
+
+        Pair(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
