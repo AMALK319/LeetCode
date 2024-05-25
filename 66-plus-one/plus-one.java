06416799872a916249734d9ef095d7bbd1cc5a57
Amal Kalim <kalimamal07@gmail.com>
2024-05-25 20:52:43 +0100

    Time: 0 ms (100.00%) | Memory: 42.3 MB (7.45%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,27 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-
-        int[] resp = new int[digits.length+1];
-
-        for(int i = digits.length-1; i>=0; i--){
-            if(digits[i] != 9) {
-                digits[i] += 1;
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
                 return digits;
-                
-            }
-            else{
-                if(i==0) resp[0] = 1;
-                digits[i] = 0;
             }
             
+            digits[i] = 0;
         }
-        for (int i = 0; i < digits.length; i++){
-            if(resp[0] == 1) resp[i+1] = digits[i];
-            else{
-                resp[i] = digits[i];
-            }
-        }
-        return resp;
+        
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
         
     }
 }
