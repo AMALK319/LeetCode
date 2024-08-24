564c9cd9b196b25bee0168a79571c6d121a496f1
Amal Kalim <kalimamal07@gmail.com>
2024-08-24 19:21:28 +0100

    Time: 1 ms (25.89%) | Memory: 41.5 MB (42.68%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.java
@@ -0,0 +1,16 @@
+/* class Solution {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");    
+    }
+} */
+
+class Solution {
+    public String defangIPaddr(String address) {
+        String defanged = "";
+        String[] splitted = address.split("\\.");
+        for(int i=0; i<splitted.length-1; i++){
+            defanged += splitted[i].concat("[.]");
+        }    
+        return defanged.concat(splitted[splitted.length-1]);
+    }
+}
