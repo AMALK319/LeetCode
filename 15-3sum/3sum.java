d1c0ab7661a37c539e7cffbe7e185181f6a02f91
Amal Kalim <kalimamal07@gmail.com>
2024-09-30 23:41:51 +0100

    Time: 32 ms (46.61%) | Memory: 52.4 MB (25.79%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -10,12 +10,12 @@ class Solution {
         for(int i = 0; i<nums.length; i++){
             
             if(nums[i] > 0 ) break;
-            //if (i>0 && nums[i] == nums[i-1]) continue;
+            if (i>0 && nums[i] == nums[i-1]) continue;
 
             int p = i+1 , q = nums.length-1;
             while(p<q){
-                //int sum = nums[p]+nums[q]+nums[i];
-                if(nums[p]+nums[q] == - nums[i]){
+                int sum = nums[p]+nums[q]+nums[i];
+                if(sum == 0){
                     temp = new ArrayList<>();
                     temp.add(nums[i]);
                     temp.add(nums[p]);
@@ -24,7 +24,7 @@ class Solution {
                     p++;
                     q--;
                 }
-                else if(nums[p]+nums[q] < - nums[i]) 
+                else if(sum < 0) 
                     p++;
                 else{
                     q--;
