aba9bace44cbf85802f5e89839d5aa8aee72ac2c
Amal Kalim <kalimamal07@gmail.com>
2024-10-02 20:49:38 +0100

    Time: 5 ms (98.07%) | Memory: 44.3 MB (90.58%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -2,42 +2,33 @@ class Solution {
     public int evalRPN(String[] tokens) {
 
         Stack<Integer> stack = new Stack<>();
-        int op = 0;
-        int op1 = 0;
-        int op2 = 0;
-        if(tokens.length == 1) return Integer.parseInt(tokens[0]); 
-        int res = 0;
-        for(int i = 0; i<tokens.length; i++){
-            try{
-                op = Integer.parseInt(tokens[i]);
-                stack.push(op);
-            }
-            catch (NumberFormatException e) {
-                op2 = stack.pop();
-                op1 = stack.pop();
-                res = operate(tokens[i], op1,op2);
-                stack.push(res);
-            }
-        }
-        return res;
-    }
 
-    private int operate(String operation, int op1, int op2){
-        int res = 0;
-        switch(operation){
+        int op1 , op2;
+
+        for(int i = 0; i<tokens.length; i++){
+            switch(tokens[i]){
             case "+": 
-                res = op1 + op2;
+                stack.push(stack.pop()+stack.pop());
                 break;
             case "*": 
-                res = op1 * op2;
+                stack.push(stack.pop()*stack.pop());
                 break; 
             case "/": 
-                res = op1 / op2;
+                op2 = stack.pop(); 
+                op1 = stack.pop();
+                stack.push(op1 / op2);
                 break;
             case "-": 
-                res = op1 - op2;
-                break;           
+                op2 = stack.pop(); 
+                op1 = stack.pop();
+                stack.push(op1 - op2);
+                break;   
+            default:
+                stack.push(Integer.parseInt(tokens[i]));
+            }
+
         }
-        return res;
+        return stack.peek();
     }
+
 }
