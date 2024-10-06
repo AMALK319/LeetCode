35ab56ece4718379f688c31a7f85206c246aff13
Amal Kalim <kalimamal07@gmail.com>
2024-10-06 14:19:39 +0100

    Time: 828 ms (54.28%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,15 +1,8 @@
-# Write your MySQL query statement below
-
-/* select p.product_id, avg(price*units) as average_price
+/* Write your PL/SQL query statement below */
+select p.product_id, ROUND(sum(units*price)/sum(units), 2) as average_price 
 from prices p, unitsSold u
 where p.product_id = u.product_id and purchase_date between start_date and end_date
-group by product_id; */
-
-select product_id, ROUND(sum(selling_price)/sum(units), 2) as average_price 
-from (  select p.product_id, start_date, purchase_date, end_date, price*units  as selling_price, units
-        from prices p, unitsSold u
-        where p.product_id = u.product_id and purchase_date between start_date and end_date) as sold
-group by product_id
+group by p.product_id
 union
 select product_id, 0
 from prices
