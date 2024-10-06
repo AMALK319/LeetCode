3969f1a81f0415993a7e027aa6d537209502d4a0
Amal Kalim <kalimamal07@gmail.com>
2024-10-06 14:29:29 +0100

    Time: 718 ms (52.31%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,15 +1,16 @@
-/* Write your PL/SQL query statement below */
-select p.product_id, ROUND(sum(units*price)/sum(units), 2) as average_price 
+# Write your MySQL query statement below
+
+/* select p.product_id, ROUND(sum(units*price)/sum(units), 2) as average_price 
 from prices p, unitsSold u
 where p.product_id = u.product_id and purchase_date between start_date and end_date
 group by p.product_id
 union
 select product_id, 0
 from prices
-where product_id not in (select product_id from UnitsSold);
+where product_id not in (select product_id from UnitsSold); */
 
 /* Solution using join */
-/* select p.product_id, COALESCE(ROUND(sum(units*price)/sum(units), 2), 0) as average_price 
+select p.product_id, COALESCE(ROUND(sum(units*price)/sum(units), 2), 0) as average_price 
 from prices p left join unitsSold u
 on p.product_id = u.product_id and purchase_date between start_date and end_date
-group by p.product_id; */
+group by p.product_id; 
