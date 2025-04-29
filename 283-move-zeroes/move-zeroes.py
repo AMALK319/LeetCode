432a8b6cb08bfbb5d2a61b59fa0086c2adb7ed1b
Amal Kalim <kalimamal07@gmail.com>
2025-04-29 23:53:51 +0100

    Time: 11 ms (37.60%) | Memory: 13.4 MB (90.90%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        slow = fast = 0
+        n = len(nums)
+        count = 0
+        for num in nums:
+            if num != 0 : count += 1
+        
+
+        while(slow<count and fast<n):
+            if(nums[slow] == 0):
+                while(nums[fast] == 0):
+                    fast += 1
+                nums[slow] = nums[fast]
+                nums[fast] = 0
+                slow += 1
+            else:
+                slow += 1
+                fast += 1
+        return nums
+            
+   
