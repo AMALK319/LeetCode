e183efb91c4ba471a8da5face399e34243b7bfa1
Amal Kalim <kalimamal07@gmail.com>
2025-04-29 21:48:05 +0100

    Time: 2 ms (67.84%) | Memory: 12.5 MB (50.72%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,19 +1,17 @@
 class Solution(object):
     def compress(self, chars):
         n = len(chars)
+        if n == 1: return 1
         s = ""
-        i = 0
-        while(i<n):
-            old = chars[i]
-            s += old
-            dup = ""
-            i += 1
-            while(i<n and chars[i]==old):
-                dup += chars[i]
-                i += 1
-            s += str(len(dup)+1) if len(dup)>0 else ""
-        
-        print(s)
+        l,f = 0,1
+        while(l<f and f<=n):
+            if(f<n and chars[f] == chars[l]):
+                f += 1
+            else:
+                s = s + chars[l] + str(f-l) if f-l>1 else s + chars[l]
+                l = f
+                f += 1
+        print (s)
 
         m = len(s)
         for i in range(m):
