566ce01dd74ef6498fed27ca8ea06312dbd5bb75
Amal Kalim <kalimamal07@gmail.com>
2025-04-30 18:13:59 +0100

    Time: 3 ms (36.33%) | Memory: 12.5 MB (45.91%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        n, m = len(s), len(t)
+        slow, fast = 0, 0
+
+        while(slow<n and fast<m):
+            if(s[slow] == t[fast]):
+                slow += 1
+            fast += 1
+
+        if slow == n: return True
+        return False
+        
+            
+        
