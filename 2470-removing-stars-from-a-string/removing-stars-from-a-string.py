deecb80064957ed1b60d86ef561ef4bfb3040652
Amal Kalim <kalimamal07@gmail.com>
2025-05-08 00:57:26 +0100

    Time: 223 ms (11.50%) | Memory: 19.5 MB (6.07%) - LeetSync

--- 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
+++ 2470-removing-stars-from-a-string/removing-stars-from-a-string.py
@@ -6,13 +6,11 @@ class Solution:
 
         i = n-1
         while i>= 0:
-            print(s[i])
             if s[i] != '*':
                 res.append(s[i])
                 i -= 1
             else:
                 while((i>= 0 and s[i] == '*') or count>0):
-                    print(s[i])
                     count = count+1 if s[i] == '*' else count-1
                     i -= 1
         
