bec87445fdd54355a0adfa60e1390eb0509e039d
Amal Kalim <kalimamal07@gmail.com>
2025-05-08 22:38:01 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (84.53%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -1,6 +1,6 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        stack = []
+        """stack = []
         s = ""
 
         i = 0
@@ -13,4 +13,13 @@ class Solution:
         while(stack):
             s += stack.pop()
 
-        return s + word[i+1:]  if i < len(word) else word   
+        return s + word[i+1:]  if i < len(word) else word  """
+
+        s = ""
+        i=0
+        while i < len(word):
+            s += word[i]
+            if word[i] == ch:
+                break
+            i += 1 
+        return s[::-1] + word[i+1:] if i < len(word) else word
