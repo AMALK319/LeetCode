a9118d95b0b3f0c96b4afa61e7fad1d545ab9ca1
Amal Kalim <kalimamal07@gmail.com>
2025-05-19 20:46:05 +0100

    Time: 7 ms (40.18%) | Memory: 18 MB (25.16%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -16,4 +16,24 @@ class Solution:
                         if (i != row and j != col) and board[row][col] == board[i][j]:
                             return False
         return True
+
+    """ Neetcode Solution: 
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
         
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if(board[r][c] in rows[r] or
+                   board[r][c] in cols[c] or
+                   board[r][c] in squares[(r//3, c//3)]):
+                   return False
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                squares[(r//3,c//3)].add(board[r][c])
+        return True """ 
+    
