0d62dab7412d38ce22ee92c6b2e81d274d242086
Amal Kalim <kalimamal07@gmail.com>
2025-05-20 18:45:24 +0100

    Time: 75 ms (16.46%) | Memory: 33 MB (24.51%) - LeetSync

--- 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.py
+++ 3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.py
@@ -1,4 +1,5 @@
 class Solution:
+    """ 
     def occurrencesOfElement(self, nums: List[int], queries: List[int], x: int) -> List[int]:
         occurencies = []
         
@@ -7,11 +8,25 @@ class Solution:
                 occurencies.append(i)
         
         answer = [0 for _ in range(len(queries))]
-        print(answer)
         for i in range(len(queries)):
             if queries[i]-1 < len(occurencies):
                 answer[i]=occurencies[queries[i]-1]
             else:
                 answer[i]=-1
+        return answer"""
+    
+    def occurrencesOfElement(self, nums: List[int], queries: List[int], x: int) -> List[int]:
+        map = {}
+        occ = 0
+        for i in range(len(nums)):
+            if nums[i] == x:
+                occ += 1
+                map[occ] = i
+        m = len(queries)
+        answer = [0 for _ in range(m)]
+        for i in range(m):
+            answer[i] = map[queries[i]] if queries[i] in map else -1
+        
         return answer
+
         
