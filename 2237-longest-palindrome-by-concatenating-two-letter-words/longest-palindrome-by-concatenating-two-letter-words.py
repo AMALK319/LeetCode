60656ea6237aef77d12da45f66cf62b4336a0c7e
Amal Kalim <kalimamal07@gmail.com>
2025-05-20 22:29:02 +0100

    Time: 311 ms (5.33%) | Memory: 36.5 MB (13.14%) - LeetSync

--- 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
+++ 2237-longest-palindrome-by-concatenating-two-letter-words/longest-palindrome-by-concatenating-two-letter-words.py
@@ -8,21 +8,18 @@ class Solution:
                 map[word] += 1
             else:
                 map[word] = 1
-        print(map)
-        
+
         answer = ""
-        wordSet = set(words)
+
         for word in words:
             if word == word[::-1]:
                 answer += word*(map[word]//2)
                 if map[word]%2 != 0 :
                     double = word
-                print(double)
             elif (word[::-1] in map):
                 answer += word*min(map[word], map[word[::-1]])
             map[word] = 0
-        print(answer)
-        print(double)
+
         reverse = answer[::-1]
         answer += double + reverse
 
