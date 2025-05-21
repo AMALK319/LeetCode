dcf7d04f1102723371ef6c8c335014f41e2f9032
Amal Kalim <kalimamal07@gmail.com>
2025-05-21 07:07:16 +0100

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.20%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.py
@@ -2,10 +2,7 @@ class Solution:
     def balancedStringSplit(self, s: str) -> int:
         ans = countR = countL = 0
         for i in range(len(s)):
-            if s[i] == 'R':
-                countR += 1
-            elif s[i] == 'L':
-                countL += 1
-            if countR == countL:
-                ans += 1
+            if s[i] == 'R': countR += 1
+            elif s[i] == 'L': countL += 1
+            if countR == countL: ans += 1
         return ans
