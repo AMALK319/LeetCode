ef631a85efa79af10e1147da3b0886d1700bd11b
Amal Kalim <kalimamal07@gmail.com>
2025-06-03 20:03:58 +0100

    Time: 590 ms (16.12%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,2 +1,2 @@
 /* Write your PL/SQL query statement below */
-select distinct(author_id) as id from views where author_id = viewer_id order by id;
+select distinct(author_id) as id from views where viewer_id = author_id order by id;
