a0593e0f4b155f489e022315fcfa70af58fd73ee
Amal Kalim <kalimamal07@gmail.com>
2025-06-18 22:01:47 +0100

    Time: 0 ms (100.00%) | Memory: 17.7 MB (55.72%) - LeetSync

--- 670-maximum-swap/maximum-swap.py
+++ 670-maximum-swap/maximum-swap.py
@@ -4,13 +4,21 @@ class Solution:
         n = len(num_str)
         max_num = num  
 
-        for i in range(n):
+        """ for i in range(n):
             for j in range(i + 1, n):
                 num_list = list(num_str)  
-                num_list[i], num_list[j] = (
-                    num_list[j],
-                    num_list[i],
-                ) 
+                num_list[i], num_list[j] = num_list[j],num_list[i] 
                 temp = int("".join(num_list))  
                 max_num = max(max_num, temp)  
-        return max_num 
+        return max_num  """
+
+        num_list = list(num_str)  
+        for i in range(n-1):
+            max_index = i
+            for j in range(n-1, i, -1):
+                if num_list[j] > num_list[max_index]:
+                    max_index = j
+            if max_index != i:
+                num_list[i], num_list[max_index] = num_list[max_index],num_list[i]
+                return int("".join(num_list))
+        return num
